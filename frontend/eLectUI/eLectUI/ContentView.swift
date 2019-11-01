828b706ae46c38d63172319b2b29f086c81f0154
Grace Economou <gecon@umich.edu>
2019-11-01 12:37:33 -0400

    creating project

---
+++ frontend/eLectUI/eLectUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  eLectUI
+//
+//  Created by Grace Economou on 11/1/19.
+//  Copyright © 2019 Grace Economou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ frontend/eLectUI/eLectUITests/eLectUITests.swift
@@ -0,0 +1,34 @@
+//
+//  eLectUITests.swift
+//  eLectUITests
+//
+//  Created by Grace Economou on 11/1/19.
+//  Copyright © 2019 Grace Economou. All rights reserved.
+//
+
+import XCTest
+@testable import eLectUI
+
+class eLectUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
