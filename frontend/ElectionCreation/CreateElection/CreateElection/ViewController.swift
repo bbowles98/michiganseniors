7d0bd0a74b7afd9ad852cc7999fd3f45fcadd973
Kevin <joohyunl@umich.edu>
2019-11-08 17:10:49 -0500

    added dismiss(animated: true, completion: nil) to search

--- frontend/ElectionCreation/CreateElection/CreateElection/ViewController.swift
+++ frontend/ElectionCreation/CreateElection/CreateElection/ViewController.swift
@@ -484,7 +484,7 @@ class SearchViewController: UIViewController {
         selectedElect = indexPath.row
         //performSegue(withIdentifier: "segue", sender: self)
         electPass = results[selectedElect]["passcode"] as! String
-        let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateInitialViewController(withIdentifier: "BallotTableView") as? BallotTableView
+        dismiss(animated: true, completion: nil)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
