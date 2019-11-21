5de225be2c6a02a7a6b5f8fbdb73d54423a9fe18
Rahul Sachdeva <rsachu@umich.edu>
2019-11-20 20:24:44 -0500

    endpoint start

--- corda/voteApp/clients/src/main/kotlin/com/template/webserver/Controller.kt
+++ corda/voteApp/clients/src/main/kotlin/com/template/webserver/Controller.kt
@@ -7,8 +7,10 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+
 
 /**
  * Define your API endpoints here.
@@ -24,13 +26,43 @@ class Controller(rpc: NodeRPCConnection) {
     private val proxy = rpc.proxy
 
     @GetMapping(value = "/home", produces = arrayOf("text/plain"))
-    private fun templateendpoint(): String {
+    private fun test(): String {
         return "Alabama, Arkansas I sure love my maw and paw, but not the way that I do love you"
     }
 
+    // register?
+
+
+
+
+    // receiving a vote
     @GetMapping(value = "/votes", produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
-    fun getIOUs() : ResponseEntity<List<StateAndRef<IOUState>>> {
+    fun storeVote() : ResponseEntity<List<StateAndRef<IOUState>>> {
         return ResponseEntity.ok(proxy.vaultQueryBy<IOUState>().states)
+    }
+
+    @PostMapping(value = "create-iou", produces = arrayOf("text/plain"), headers = arrayOf("Content-Type=application/x-www-form-urlencoded"))
+    fun createIOU(request: HttpServletRequest): ResponseEntity<String> {
+        val iouValue = request.getParameter("iouValue").toInt()
+        val partyName = request.getParameter("partyName")
+        return partyName
+//        if(partyName == null){
+//            return ResponseEntity.badRequest().body("Query parameter 'partyName' must not be null.\n")
+//        }
+//        if (iouValue <= 0 ) {
+//            return ResponseEntity.badRequest().body("Query parameter 'iouValue' must be non-negative.\n")
+//        }
+//        val partyX500Name = CordaX500Name.parse(partyName)
+//        val otherParty = proxy.wellKnownPartyFromX500Name(partyX500Name) ?: return ResponseEntity.badRequest().body("Party named $partyName cannot be found.\n")
+//
+//        return try {
+//            val signedTx = proxy.startTrackedFlow(::Initiator, iouValue, otherParty).returnValue.getOrThrow()
+//            ResponseEntity.status(HttpStatus.CREATED).body("Transaction id ${signedTx.id} committed to ledger.\n")
+//
+//        } catch (ex: Throwable) {
+//            logger.error(ex.message, ex)
+//            ResponseEntity.badRequest().body(ex.message!!)
+//        }
     }
 
 
