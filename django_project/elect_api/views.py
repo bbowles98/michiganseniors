268104ddcc5fe1d5ee5396ceb5471f355e434f4b
pchiapuz <pchiapuz@umich.edu>
2019-11-24 17:35:48 -0500

    Fixed Json error in results

--- django_project/elect_api/views.py
+++ django_project/elect_api/views.py
@@ -8,6 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from rest_framework.response import Response
 from rest_framework import filters
 from django.db import connection
 from datetime import datetime
