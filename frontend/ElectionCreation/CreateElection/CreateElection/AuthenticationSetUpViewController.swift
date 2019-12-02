527bcaad6f8b7959db4c4a9e66fa4df41e5fec49
Grace Economou <gecon@umich.edu>
2019-12-02 07:44:40 -0500

    voting options should be working

--- frontend/ElectionCreation/CreateElection/CreateElection/AuthenticationSetUpViewController.swift
+++ frontend/ElectionCreation/CreateElection/CreateElection/AuthenticationSetUpViewController.swift
@@ -26,7 +26,57 @@ class AuthenticationSetUpViewController: UIViewController {
     var electID:String = ""
     
     @IBAction func onNext(_ sender: Any) {
+        
+        let json: [String: Any] = [
+            "election_id": self.electID,
+            "max_voters": numVoters,
+            "email_domain": allowedDomain
+        ]
+              
+              
+      let jsonData = try? JSONSerialization.data(withJSONObject: json)
+      var request = URLRequest(url: URL(string: "http://204.48.30.178/addElectionRestrictions/")!)
+      request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+      request.addValue("application/json", forHTTPHeaderField: "Accept")
+      
+      request.addValue("JWT " + token, forHTTPHeaderField: "Authorization")
+      print("ballot token:")
+      print(token)
+      request.httpMethod = "POST"
+      request.httpBody = jsonData
+      //print("jsonData 51: ")
+      
+      if let string = String(bytes: jsonData!, encoding: .utf8) {
+          print(string)
+      }
+      
+      //async error handling
+      let task = URLSession.shared.dataTask(with: request) { data, response, error in
+          guard let _ = data, error == nil else {
+              
+              print("NETWORKING ERROR")
+              return
+          }
+          
+          if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+              
+              print(response.debugDescription)
+              print("HTTP STATUS: \(httpStatus.statusCode)")
+              return
+          }
+          do {
+              let json = try JSONSerialization.jsonObject(with: data!) as! [String:Any]
+
+          }
+          catch let error as NSError {
+              print(error)
+          }
+      }
+      //run the previous copule lines of code in a seperate thread
+        task.resume()
+        
         performSegue(withIdentifier: "ToAddOption", sender: (Any).self)
+        
     }
     @IBAction func onBack(_ sender: Any) {
         performSegue(withIdentifier: "BackToCreate", sender: (Any).self)
